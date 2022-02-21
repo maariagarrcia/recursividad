f5fedf9a9a28dad0683eada090221f8af823a1ec
maariagarrcia <mrrmn@myuax.com>
2022-02-21 18:58:21 +0100

    main +  estructura programa

---
+++ main.py
@@ -0,0 +1,20 @@
+###  I  M P O R T S
+from colorama import Fore
+
+
+#
+#   F U N C I O N E S
+#
+
+def mostrar_resultado(descripcion, resultado):
+    print(Fore.WHITE + "> " + descripcion + Fore.YELLOW, resultado, Fore.WHITE)
+
+
+
+def busqueda_dicotomica():
+    
+
+
+#
+#   I N I C I O   P R O G R A M A
+#
