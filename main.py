96756ac3663109f9f7b89575fef9e98acfe569a0
maariagarrcia <mrrmn@myuax.com>
2022-02-22 15:56:20 +0100

    correcciones

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 ###  I  M P O R T S
 from colorama import *
-
+from sortedlist_class import *
+import helpers
+from menu_class import *
 
 #
 #   F U N C I O N E S
@@ -11,10 +13,10 @@ def mostrar_resultado(descripcion, resultado):
 
 
 
-def busqueda_dicotomica():
+def busqueda_dicotomica():  
     
-
 
 #
 #   I N I C I O   P R O G R A M A
 #
+
